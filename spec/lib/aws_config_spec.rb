52b2c13baec310454af8180d90d704ada972e6d0
Kierran McPherson <kierranm@gmail.com>
2016-05-01 12:48:41 +1200

    Update specs to test profile chains
    Its just region not aws_region

--- spec/lib/aws_config_spec.rb
+++ spec/lib/aws_config_spec.rb
@@ -2,7 +2,11 @@ require "spec_helper"
 
 describe AWSConfig do
   let(:sample_config_file) { File.expand_path("../../samples/config.txt", __FILE__) }
-  before { AWSConfig.config_file = sample_config_file }
+  let(:sample_creds_file) { File.expand_path("../../samples/credentials.txt", __FILE__) }
+  before do
+    AWSConfig.config_file = sample_config_file
+    AWSConfig.credentials_file = sample_creds_file
+  end
 
   it "should return an entry in a profile via method" do
     expect(described_class.default.aws_access_key_id).to eq "DefaultAccessKey01"
